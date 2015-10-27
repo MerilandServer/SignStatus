965d279886ba21052e6763d338c371ab5162fb53
Cadiducho <cadiducho@gmail.com>
2015-10-27 18:49:00 +0100

    Añadida obtención de fecha de desconexión desde Essentials

--- src/com/syteck/signstatus/utils/SignUtil.java
+++ src/com/syteck/signstatus/utils/SignUtil.java
@@ -1,18 +1,27 @@
 package com.syteck.signstatus.utils;
 
+import com.earth2me.essentials.IEssentials;
+import com.earth2me.essentials.User;
 import java.util.Date;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
 import com.syteck.signstatus.Storage;
+import org.bukkit.Bukkit;
 
 public class SignUtil {
 
 	public static String process(String str, String name) {
-
 		String date = Storage.getDateFormat().format(new Date());
 
+                IEssentials es = (IEssentials) Bukkit.getPluginManager().getPlugin("Essentials");
+                User u2 = es.getOfflineUser(name);
+                if (u2 != null) { //Si existe, obtener su fecha desde Essentials
+                    Date fechaEssentials = new Date();
+                    fechaEssentials.setTime(u2.getLastLogout());
+                    date = Storage.getDateFormat().format(fechaEssentials);
+                }
 		return str.replaceAll("%date%", date).replaceAll("%name%", name);
 	}
 
